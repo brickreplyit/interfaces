515ace513ffe93983cfdab696fe9f4fbc748d05d
Musso Pierluca <p.musso@reply.it>
2019-04-12 17:54:48 +0200

    MQTT WORKS

--- test/integration/mqtt.spec.js
+++ test/integration/mqtt.spec.js
@@ -1,33 +1,66 @@
 const expect = require('chai').expect;
+const utilities_execution_work = require('../unit/utility.executionwork');
+const implementations = require('../../src/Core/Index');
 const common = require('../../src/App/common/common');
 const mqtt = require('../../src/App/infrastructure/mqtt/mqtt');
 const config = global.gConfig;
 const conn_string = config.test.integration.MQTT_ENDPOINT;
 
-describe('MQTT', () => {
-    it('Should receive a Message', () => {
-        const MQTT_BROKER = new mqtt(common.MQTT_LOGGER, conn_string);
+describe('INTEGRATION TESTS', () => {
+    describe('MQTT SERVER', () => {
+        it('Should receive a Message', () => {
+            const MQTT_BROKER = new mqtt(common.MQTT_LOGGER, conn_string);
 
-        MQTT_BROKER.Init(['TEST']);
+            MQTT_BROKER.Init(['TEST']);
 
-        MQTT_BROKER.RegisterMessageLogic((topic, message) => {
-            expect(message.toString()).to.be.eq(JSON.stringify({ TYPE: 'TEST' }));
-            expect(topic).to.be.eq('TEST');
-            MQTT_BROKER.End();
+            MQTT_BROKER.RegisterMessageLogic((topic, message) => {
+                expect(message.toString()).to.be.eq(JSON.stringify({ TYPE: 'TEST' }));
+                expect(topic).to.be.eq('TEST');
+                MQTT_BROKER.End();
+            });
+
+            MQTT_BROKER.Publish('TEST', { TYPE: 'TEST' });
         });
 
-        MQTT_BROKER.Publish('TEST', { TYPE: 'TEST' });
+        it('Should receive a Message and Close the connection', () => {
+            const MQTT_BROKER = new mqtt(common.MQTT_LOGGER, conn_string);
+
+            MQTT_BROKER.Init(['TEST']);
+
+            MQTT_BROKER.RegisterMessageLogic(() => {
+                MQTT_BROKER.End();
+            });
+
+            MQTT_BROKER.Publish('TEST', { TYPE: 'TEST' });
+        });
     });
 
-    it('Should receive a Message and Close the connection', () => {
-        const MQTT_BROKER = new mqtt(common.MQTT_LOGGER, conn_string);
+    describe('PLANT COMMUNICATION WITH DEVICES', () => {
 
-        MQTT_BROKER.Init(['TEST']);
+        it('Should work with sequential children MQTT', async function() {
+        
+            const test_tables =  await utilities_execution_work.TestFactoryMQTT(1440 * 60 * 1000, implementations.SequentialExecutionWork);
+         
+            const start_unit = 1;
 
-        MQTT_BROKER.RegisterMessageLogic(() => {
-            MQTT_BROKER.End();
+            expect(test_tables.stock_table[test_tables.start + ((2 + start_unit) * test_tables.time_unit)].outAvv).to.be.eq(200);
+
+            expect(test_tables.station_table[test_tables.start + ((2 + start_unit) * test_tables.time_unit)].AVV).to.be.eq(200);
+
+            expect(test_tables.station_table[test_tables.start + ((4 + start_unit) * test_tables.time_unit)].AVV).to.be.eq(200);
+
+            expect(test_tables.warehouse_final[test_tables.start + ((4 + start_unit) * test_tables.time_unit)].Finished).to.be.eq(1000);
+
         });
 
-        MQTT_BROKER.Publish('TEST', { TYPE: 'TEST' });
-    });
+        it('Should Work with parallel children MQTT', async () => {
+            const test_tables =  await utilities_execution_work.TestFactoryMQTT(0, implementations.ParallelExecutionWork);
+         
+            const start_unit = 1;
+        });
+
+    });  
+
 });
+
+
--- test/unit/plant.spec.js
+++ test/unit/plant.spec.js
@@ -46,22 +46,6 @@ describe('Plant', () => {
 
     });
 
-    it('Should work with sequential children MQTT', async function() {
-        
-        const test_tables =  await utilities_execution_work.TestFactoryMQTT(1440 * 60 * 1000, implementations.SequentialExecutionWork);
-         
-        const start_unit = 1;
-
-        expect(test_tables.stock_table[test_tables.start + ((2 + start_unit) * test_tables.time_unit)].outAvv).to.be.eq(200);
-
-        expect(test_tables.station_table[test_tables.start + ((2 + start_unit) * test_tables.time_unit)].AVV).to.be.eq(200);
-
-        expect(test_tables.station_table[test_tables.start + ((4 + start_unit) * test_tables.time_unit)].AVV).to.be.eq(200);
-
-        expect(test_tables.warehouse_final[test_tables.start + ((4 + start_unit) * test_tables.time_unit)].Finished).to.be.eq(1000);
-
-    });
-
     it('Should Work with parallel children', async () => {
         const test_tables =  await utilities_execution_work.TestFactory(0, implementations.ParallelExecutionWork);
          
